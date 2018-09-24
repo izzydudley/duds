13ac959de6c088b5867439fa5499235fb3008cab
izzydudley <bieberswag9014@gmail.com>
2018-09-24 17:36:39 +0100

    changes
    i made a function within the classes to set up the cars and changed the remove cars function....
    but im still having trouble with certain cars being out of range when they've moved even with seperate column and row validation functions?

--- CARS OOP.py
+++ CARS OOP.py
@@ -22,123 +22,68 @@ def PrintBoard(Board):
 
 
 class Cars:
-    def __init__(self, Car, Orientation, Row, Column, Board):
-        self.Car = Car
+    def __init__(self, Colour, Size, Orientation, Row, Column, Board):
+        self.Colour = Colour
+        self.Size = Size
         self.Orientation = Orientation
         self.Row = Row
         self.Column = Column
+        self.RunCars(Board)
 
-        SetCars(Board, Row, Column, Car)
+    def RunCars(self,Board):
+        if self.Orientation == "H":
+            for i in range(self.Size):
+                Board[self.Row][self.Column+i] = self.Colour
+        else:
+            for i in range(self.Size):
+                Board[self.Row+i][self.Column] = self.Colour
 
-    def MoveCar(self, Board, Car):
+    def MoveCar(self, Board, Colour):
         if self.Orientation == "H":
             Column2 = int(input("Enter moving Column: "))
             while Column2 < 0 or Column2 > 5:
                Column2 = int(input("Enter moving Column: "))
             else:
-                Column = Column2
-                #Column = Column2 - 1
-                #if Car== "PURPLE":
-                 #   Column -= 1
+                self.Column = Column2
                 Row = self.Row
         else:
             Row2 = int(input("Enter moving Row: "))
             while Row2 < 0 or Row2 > 5:
                 Row2 = int(input("Enter moving Row: "))
-            Row = Row2
-            #Row = Row2 - 1
-            #if Car == "YELLOW" or Car == "BLUE":
-                #Row -= 1
+            self.Row = Row2
             Column = self.Column
 
-        Set = ValidateCars(self ,Board, Row, Column)
+
+            
+        CheckColumn(Colour,self.Column)
+        CheckRow(Colour, self.Row)
+        Set = ValidateCars(self ,Board, self.Row, self.Column)
         if Set == True:
-            RemoveCars(Board,self.Row,self.Column,self.Car)
-            self.Row = Row
-            self.Column = Column
-            SetCars(Board, Row, Column, self.Car)
+            RemoveCars(Board,self.Colour)
+            self.RunCars(Board)
             
+            
+def CheckColumn(Colour,Column):
+    print("HELLOE")
+    if Column+1 > 5 or Column+2 > 5:
+        Column = 3
+
+
+def CheckRow(Colour,Row):
+    if Row+1 > 5 or Row+2 > 5:
+        Row = 3
+
+def RemoveCars(Board,Colour):
+    for i in range(len(Board)):
+        for ii in range(len(Board[i])):
+            if Board[i][ii] == Colour[0]:
+                Board[i][ii] = "_"
         
-
-
-def RemoveCars(Board,Row,Column,Car):
-    if Car == "RED":
-            Board[Row][Column] = "_"
-            Board[Row][Column+1] = "_"
-
-    elif Car =="PURPLE":
-            Board[Row][Column] = "_"
-            Board[Row][Column+1] = "_"
-            Board[Row][Column+2] = "_"
-
-
-    elif Car== "YELLOW":
-            Board[Row][Column] = "_"
-            Board[Row+1][Column] = "_"
-            Board[Row+2][Column] = "_"
-            
-
-    elif Car == "GREEN":
-            Board[Row][Column] = "_"
-            Board[Row][Column+1] = "_"
-            
-
-    elif Car == "BLUE":
-        Board[Row][Column] = "_"
-        Board[Row+1][Column] = "_"
-        Board[Row+2][Column] = "_"
-                       
-    else:
-        print("Invalid car")
-
-    
-
-def SetCars(Board, Row, Column, Car):
-    if Car == "RED":
-                if Column+1 > 5:
-                    Column = 4
-                Board[Row][Column] = "R"
-                Board[Row][Column+1] = "R"
-
-    elif Car =="PURPLE":
-            if Column+1 > 5 or Column+2 > 5:
-                Column = 3
-            Board[Row][Column] = "P"
-            Board[Row][Column+1] = "P"
-            Board[Row][Column+2] = "P"
-
-
-    elif Car== "YELLOW":
-            if Row+1 > 5 or Row+2 > 5:
-                Row = 3
-            Board[Row][Column] = "Y"
-            Board[Row+1][Column] = "Y"
-            Board[Row+2][Column] = "Y"
-            
-
-    elif Car == "GREEN":
-            if Column+1 > 5:
-                Column = 4
-            Board[Row][Column] = "G"
-            Board[Row][Column+1] = "G"
-            
-
-    elif Car == "BLUE":
-        if Row+1 > 5 or Row+2 > 5:
-            Row = 3
-        Board[Row][Column] = "B"
-        Board[Row+1][Column] = "B"
-        Board[Row+2][Column] = "B"
-                       
-    else:
-        print("Invalid car")
-
-        
-def ValidateCars(Car, Board, Row, Column):
+def ValidateCars(Colour, Board, Row, Column):
     
     if Board[Row][Column] in ["Y","B","P","G","R"]:
         print("Invalid Coords")
-        Car.MoveCar(Board, Car)
+        Colour.MoveCar(Board, Colour)
     
 
         return False
@@ -150,30 +95,30 @@ def ValidateCars(Car, Board, Row, Column):
            
 Board = SetBoard()       
 
-Red = Cars("RED", "H", 2,0, Board)
-Purple = Cars("PURPLE", "H", 3,0, Board)
-Yellow = Cars("YELLOW", "V", 0,2, Board)
-Green = Cars("GREEN", "H", 0,4, Board)
-Blue = Cars("BLUE", "V", 3,5, Board)
+Red = Cars("R", 2, "H", 2,0, Board)
+Purple = Cars("P", 3, "H", 3,0, Board)
+Yellow = Cars("Y", 3, "V", 0,2, Board)
+Green = Cars("G", 2, "H", 0,4, Board)
+Blue = Cars("B", 3, "V", 3,5, Board)
 
 while Board[2][5] != ["R"]:
 
     PrintBoard(Board)
-    car = input("Enter a car u want to move: ").upper()
-    while car not in ["RED","PURPLE", "YELLOW","GREEN","BLUE"]:
-        car = input("Enter a car u want to move: ").upper()
-    Car = car
+    colour = input("Enter a colour u want to move: ").upper()
+    while colour not in ["R","P", "Y","G","B"]:
+        colour = input("Enter a colour u want to move: ").upper()
+    Colour = colour
         
-    if car == "RED":
-        Red.MoveCar(Board, Car)
-    elif car == "PURPLE":
-        Purple.MoveCar(Board, Car)
-    elif car == "YELLOW":
-        Yellow.MoveCar(Board, Car)
-    elif car == "GREEN":
-        Green.MoveCar(Board, Car)
-    elif car == "BLUE":
-        Blue.MoveCar(Board, Car)
+    if colour == "R":
+        Red.MoveCar(Board, Colour)
+    elif colour == "P":
+        Purple.MoveCar(Board, Colour)
+    elif colour == "Y":
+        Yellow.MoveCar(Board, Colour)
+    elif colour == "G":
+        Green.MoveCar(Board, Colour)
+    elif colour == "B":
+        Blue.MoveCar(Board, Colour)
     else:
         print("shouldnt get here")
 
