52e18cba3ce1d26118c9817b35c8af9b1bcc62e4
izzydudley <bieberswag9014@gmail.com>
2018-09-25 12:27:11 +0100

    More...
    chnged it some more now! no more issues with cars being out of range. the new issue now is the cars can jump over each other.. not quite sure how to solve that..

--- CARS OOP.py
+++ CARS OOP.py
@@ -33,45 +33,55 @@ class Cars:
     def RunCars(self,Board):
         if self.Orientation == "H":
             for i in range(self.Size):
+                if self.Column + i > 5:
+                    i *= -1
                 Board[self.Row][self.Column+i] = self.Colour
         else:
             for i in range(self.Size):
+                if self.Row + i > 5:
+                    i *= -1
                 Board[self.Row+i][self.Column] = self.Colour
 
     def MoveCar(self, Board, Colour):
         if self.Orientation == "H":
-            Column2 = int(input("Enter moving Column: "))
-            while Column2 < 0 or Column2 > 5:
-               Column2 = int(input("Enter moving Column: "))
-            else:
-                self.Column = Column2
+            tap = False
+            Column2 = input("Enter Column: ")
+            if Column2.isdigit():
+                Column2 = int(Column2)
+                while Column2 < 0 or Column2 > 5:
+                    Column2 = int(input("Enter moving Column: "))
+                self.Column = int(Column2)
                 Row = self.Row
+                tap = True
+                while tap == False:
+                    Column2 = input("Enter Column: ")
+                
         else:
-            Row2 = int(input("Enter moving Row: "))
-            while Row2 < 0 or Row2 > 5:
-                Row2 = int(input("Enter moving Row: "))
-            self.Row = Row2
-            Column = self.Column
+            top = False
+            Row2 = input("Enter moving Row: ")
+            if Row2.isdigit():
+                Row2 = int(Row2)
+                while Row2 < 0 or Row2 > 5:
+                    Row2 = int(input("Enter moving Row: "))
+                self.Row = Row2
+                Column = self.Column
+                top = True
+                while top == False:
+                    Row2 = int(input("Enter moving Row: "))
+                    
 
-
-            
-        CheckColumn(Colour,self.Column)
-        CheckRow(Colour, self.Row)
         Set = ValidateCars(self ,Board, self.Row, self.Column)
         if Set == True:
             RemoveCars(Board,self.Colour)
             self.RunCars(Board)
             
-            
-def CheckColumn(Colour,Column):
-    print("HELLOE")
-    if Column+1 > 5 or Column+2 > 5:
-        Column = 3
+#def CheckCars(Board, Column, Row, Colour, Orientation):
+    #make a row into an array and iterate throu, find colour using,
+    #iterate from that until get till anotehr colour
+    # change underscore to lowcase of given colour.
+    #iterate from lowercase to uppercase.
+    
 
-
-def CheckRow(Colour,Row):
-    if Row+1 > 5 or Row+2 > 5:
-        Row = 3
 
 def RemoveCars(Board,Colour):
     for i in range(len(Board)):
@@ -89,8 +99,7 @@ def ValidateCars(Colour, Board, Row, Column):
         return False
     return True
         
-        
-        
+                
     
            
 Board = SetBoard()       
@@ -101,8 +110,7 @@ Yellow = Cars("Y", 3, "V", 0,2, Board)
 Green = Cars("G", 2, "H", 0,4, Board)
 Blue = Cars("B", 3, "V", 3,5, Board)
 
-while Board[2][5] != ["R"]:
-
+while Board[2][5] != "R":
     PrintBoard(Board)
     colour = input("Enter a colour u want to move: ").upper()
     while colour not in ["R","P", "Y","G","B"]:
@@ -121,6 +129,8 @@ while Board[2][5] != ["R"]:
         Blue.MoveCar(Board, Colour)
     else:
         print("shouldnt get here")
+
+print("YOU WIN")
 
 PrintBoard(Board)
 
